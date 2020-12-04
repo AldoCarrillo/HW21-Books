5c7e44321b7d51e1dcbec62c481b1623ba77213d
Aldo Carrillo <aldo.carrillo22@gmail.com>
2020-12-03 21:55:32 -0800

    jason packge update

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ app.use(express.json());
 app.use(cors());
 app.use('/app', routesUrls);
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('build'));
+}
+
 app.listen(PORT, () => {
 	console.log(`App running on port ${PORT}!`);
 });
